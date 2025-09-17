f93e14ca17f733afe885d4025f599e4a0023f0f2
BarrozoL <barrozo.lucasp@gmail.com>
2025-09-17 17:52:26 +0100

    blog post changes

--- src/app/components/BlogSidebar.jsx
+++ src/app/components/BlogSidebar.jsx
@@ -1,33 +1,28 @@
 "use client";
 
 import Image from "next/image";
-
 import React from "react";
 import Link from "next/link";
 import { blogPosts } from "./BlogData";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
-  const recentPosts = blogPosts.slice(0, 3);
+  const [recentPosts, setRecentPosts] = useState(blogPosts.slice(0, 4));
+
+  useEffect(() => {
+    // Shuffle apenas no cliente
+    const shuffled = [...blogPosts].sort(() => 0.5 - Math.random()).slice(0, 4);
+    setRecentPosts(shuffled);
+  }, []);
 
   return (
-    <aside className="w-full lg:w-72 lg:ml-8">
-      {/* "Compartilhe" Section */}
-      {/*   <div className="pb-4 mb-4 border-b border-gray-300">
-        <h3 className="text-xl font-bold mb-4">Compartilhe</h3>
-        <div className="flex space-x-4">
-          <SocialIcon icon="facebook-f" link="https://facebook.com" />
-          <SocialIcon icon="twitter" link="https://twitter.com" />
-          <SocialIcon icon="linkedin-in" link="https://linkedin.com" />
-        </div>
-      </div> */}
-
+    <aside className="w-full">
       {/* Mais Artigos Section */}
       <div>
-        <h3 className="text-x1 font-bold mb-4">Mais Artigos</h3>
-        <ul className="space-y-9">
+        <h3 className="text-xl font-bold mb-4">Mais Artigos</h3>
+        <ul className="space-y-6">
           {recentPosts.map((post) => (
             <li key={post.id} className="flex items-start space-x-4">
-              {/* Optional Image - if your post has an `imagem` field */}
               {post.imagem ? (
                 <div className="w-20 h-16 flex-shrink-0 bg-gray-200 overflow-hidden rounded">
                   <Image
@@ -38,14 +33,14 @@ export default function Sidebar() {
                 </div>
               ) : null}
 
-              <div>
+              <div className="flex-1">
                 <Link
                   href={`/blog/${post.slug}`}
-                  className="text-lg font-semibold text-gray-900 hover:underline"
+                  className="text-lg font-semibold text-gray-700 hover:underline"
                 >
                   {post.titulo}
                 </Link>
-                <p className="text-sm w-60 text-gray-600 mt-1 line-clamp-3">
+                <p className="text-sm text-gray-600 mt-1 line-clamp-3">
                   {post.excerpt}
                 </p>
               </div>
